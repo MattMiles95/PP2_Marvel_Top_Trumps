04d9329ccfc925cdc2212e0c7de5006f26813b7d
Matt Miles <matt_miles2@live.co.uk>
2024-05-01 15:03:17 +0100

    Add functionality to homepage buttons

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Homepage
+
+/**
+ * Script to display and close User's Deck Choice window
+ */
+let playBtn = document.getElementById("play-button");
+let deckChoiceContainer = document.getElementById("deck-choice-container");
+let closeWindow = document.getElementById("close-window");
+
+playBtn.addEventListener("click", () => {
+    deckChoiceContainer.style.display = "flex";
+})
+
+closeWindow.addEventListener("click", () => {
+    deckChoiceContainer.style.display = "none";
+})
