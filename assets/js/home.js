8c2bafb1788c741d7cb446e90de87c0ae381658b
Matt Miles <matt_miles2@live.co.uk>
2024-05-06 13:51:00 +0100

    Update home.js comments

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,14 +1,17 @@
-/**
- * Script to display and close User's Deck Choice window
- */
 let playBtn = document.getElementById("play-button");
 let deckChoiceContainer = document.getElementById("deck-choice-container");
 let closeWindow = document.getElementsByClassName("close-window")[0];
 
+/**
+ * Opens User's Deck Choice window
+ */
 playBtn.addEventListener("click", () => {
     deckChoiceContainer.style.display = "flex";
 })
 
+/**
+ * Closes User's Deck Choice window
+ */
 closeWindow.addEventListener("click", () => {
     deckChoiceContainer.style.display = "none";
 });
